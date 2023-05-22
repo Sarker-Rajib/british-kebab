2d5606a24996670f4f7735a7ae686cc6bcdec1e9
Rajib Sarker <redhat0593@gmail.com>
2023-05-23 01:45:09 +0600

    pro-d

---
+++ assets/js/custom.js
@@ -0,0 +1,48 @@
+var swiper = new Swiper(".mini-menu", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    // freeMode: true,
+    loop: true,
+    // centeredSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        // el: ".swiper-pagination",
+        clickable: true,
+
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 40
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 6,
+            spaceBetween: 20
+        },
+    }
+});
+
+//  logo slider
+var swiper = new Swiper(".item-ddetails", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    // freeMode: true,
+    loop: true,
+    // centeredSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        // el: ".swiper-pagination",
+        clickable: true,
+
+    }
+});
