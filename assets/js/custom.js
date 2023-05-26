3379099b967087c443f992b1458cb652a47928f6
Rajib Sarker <redhat0593@gmail.com>
2023-05-26 14:36:47 +0600

    ah7shwlwss

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,21 @@
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("header").style.background = "rgb(0 0 0 / 25%)";
+        document.getElementById("header").style.backdropFilter = "blur(15px)";
+        document.getElementById("logooo").style.width = "100px";
+    } else {
+        document.getElementById("header").style.background = "none";
+        document.getElementById("header").style.backdropFilter = "none";
+        document.getElementById("logooo").style.width = "auto";
+    }
+}
+
+
+
 var swiper = new Swiper(".mini-menu", {
     slidesPerView: 6,
     spaceBetween: 30,
@@ -31,6 +49,22 @@ var swiper = new Swiper(".mini-menu", {
 });
 
 //  logo slider
+var swiper = new Swiper(".item-ddetails", {
+    slidesPerView: 1,
+    // freeMode: true,
+    loop: true,
+    // centeredSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        // el: ".swiper-pagination",
+        clickable: true,
+    }
+});
+
+//  item1
 var swiper = new Swiper(".item-ddetails", {
     slidesPerView: 1,
     // freeMode: true,
