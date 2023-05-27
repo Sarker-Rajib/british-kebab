2bb40bacb4e336bc2b06a98ff40b9778064aac22
Rajib Sarker <redhat0593@gmail.com>
2023-05-27 18:21:21 +0600

    alkus

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,18 @@
+AOS.init();
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("header").style.background = "rgb(0 0 0 / 25%)";
+        document.getElementById("header").style.backdropFilter = "blur(15px)";
+        document.getElementById("header").style.padding = "10px 0";
+    } else {
+        document.getElementById("header").style.background = "none";
+        document.getElementById("header").style.backdropFilter = "none";
+        document.getElementById("header").style.padding = "20px 0";
+    }
+}
 
 // mini slider
 var swiper = new Swiper(".mini-menu", {
@@ -55,13 +70,21 @@ var swiper = new Swiper(".menu1", {
     },
     pagination: {
         clickable: true,
-    }
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
 });
 
 // menu card slider 2
 var swiper = new Swiper(".menu2", {
     slidesPerView: 1,
     loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -75,6 +98,10 @@ var swiper = new Swiper(".menu2", {
 var swiper = new Swiper(".menu3", {
     slidesPerView: 1,
     loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -88,6 +115,10 @@ var swiper = new Swiper(".menu3", {
 var swiper = new Swiper(".menu4", {
     slidesPerView: 1,
     loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
